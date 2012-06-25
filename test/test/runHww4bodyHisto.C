3b7f000a1a4b810dc3566afc5a8ad062f707e003
zixu <>
2012-06-25 14:06:06 +0000

    *** empty log message ***

--- test/test/runHww4bodyHisto.C
+++
@@ -1,6 +0,0 @@
-{
-  gROOT->ProcessLine(".L RooWjjFitterParams.h+");
-  gROOT->ProcessLine(".L RooWjjFitterUtils.cc+");
-  gROOT->ProcessLine(".L RooWjjMjjFitter.cc+");
-  gROOT->ProcessLine(".x RooWjj4BodyFitter.C+");
-}
